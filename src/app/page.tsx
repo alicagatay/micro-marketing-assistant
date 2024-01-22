37a6583a346dd2031ab846c54ee0d37fed7a609b
Ali Cagatay <aliccagatay@gmail.com>
2024-01-22 16:54:35 +0000

    Update the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
-
-export default function HomePage() {
+export default async function HomePage() {
   return (
-
+    <div className="flex h-screen items-center justify-center">
+      <div className="text-4xl">Hello World</div>
+    </div>
   );
 }
