bbf4634960ba42444c90e56ab4b5bfe3a3980279
Ali Cagatay <aliccagatay@gmail.com>
2024-01-24 15:23:00 +0000

    Delete the old API route.

--- src/app/api/route.ts
+++
@@ -1,8 +0,0 @@
-import { clerkClient, currentUser } from "@clerk/nextjs";
-
-export async function GET(request: Request) {
-  const user = await currentUser();
-  const users = await clerkClient.users.getUserList();
-
-  return new Response(JSON.stringify(user));
-}
