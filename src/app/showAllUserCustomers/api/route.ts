4bae71a40a61899fb70ce2bc97561dcd727df4d4
Ali Cagatay <aliccagatay@gmail.com>
2024-01-24 16:17:23 +0000

    Add `showAllUserCustomers` API route to show all user customers.

---
+++ src/app/showAllUserCustomers/api/route.ts
@@ -0,0 +1,15 @@
+import { clerkClient, currentUser } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+
+export async function GET(request: Request) {
+  const prisma = new PrismaClient();
+  const user = await currentUser();
+
+  const allUserProducts = await prisma.customer.findMany({
+    where: {
+      ownerID: user?.id,
+    },
+  });
+
+  return new Response(JSON.stringify(allUserProducts));
+}
