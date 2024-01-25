33d45cf1fb1bd306809f07b7b57db134aa1399d4
Ali Cagatay <aliccagatay@gmail.com>
2024-01-25 13:34:16 +0000

    Add the table screen component to the home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import TableScreen from "./tableScreen";
+
 export default function HomePage() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="text-6xl">Hello World</div>
-    </div>
-  );
+  return <TableScreen />;
 }
