612310866a7a876d4674ff4ee5daaa4c1e3cb02e
Ali Cagatay <aliccagatay@gmail.com>
2024-01-29 17:03:58 +0000

    Remove `FilterScreen` component from `HomePage`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import FilterScreen from "./filterScreen";
 import TableScreen from "./tableScreen";
 
 export default function HomePage() {
-  return (
-    <div className="flex h-screen flex-col">
-      <FilterScreen />
-      <TableScreen />
-    </div>
-  );
+  return <TableScreen />;
 }
