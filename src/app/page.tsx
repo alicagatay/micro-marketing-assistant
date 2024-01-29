7948ef0e02ef09dc027c4a79facd233cb1757cb6
Ali Cagatay <aliccagatay@gmail.com>
2024-01-29 16:07:48 +0000

    Add `FilterScreen` component to `HomePage`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import FilterScreen from "./filterScreen";
 import TableScreen from "./tableScreen";
 
 export default function HomePage() {
   return (
     <div className="flex h-screen flex-col">
+      <FilterScreen />
       <TableScreen />
     </div>
   );
