3cfb6ad1b7fc54d305bad294d1145c7e6b5ff3e0
Ali Cagatay <aliccagatay@gmail.com>
2024-01-29 15:15:02 +0000

    Update the height of the TableScreen component.

--- src/app/tableScreen.tsx
+++ src/app/tableScreen.tsx
@@ -38,7 +38,7 @@ export default function TableScreen() {
   }
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8">
+    <div className="h-5/6 overflow-auto px-4 sm:px-6 lg:px-8">
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle">
