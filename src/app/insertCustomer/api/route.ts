372407d39e75f6dcb39527eda1b485c1130ca363
Ali Cagatay <aliccagatay@gmail.com>
2024-01-31 17:16:09 +0000

    Add Customer type definition.

---
+++ src/app/insertCustomer/api/route.ts
@@ -0,0 +1,11 @@
+type Customer = {
+  name: string;
+  email: string;
+  mobileNumber: string;
+  country: string;
+  city: string;
+  company: string;
+  jobTitle: string;
+  targetProduct: string;
+  ownerID: string;
+};
