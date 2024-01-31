aea0ad27f5593cc8b772d77a3ea154a53792af96
Ali Cagatay <aliccagatay@gmail.com>
2024-01-31 18:00:53 +0000

    Add customer creation API endpoint

--- src/app/insertCustomer/api/route.ts
+++ src/app/insertCustomer/api/route.ts
@@ -1,3 +1,6 @@
+import { clerkClient, currentUser } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+
 type Customer = {
   name: string;
   email: string;
@@ -9,3 +12,42 @@ type Customer = {
   targetProduct: string;
   ownerID: string;
 };
+
+type requestBody = {
+  name: string;
+  email: string;
+  mobileNumber: string;
+  country: string;
+  city: string;
+  company: string;
+  jobTitle: string;
+  targetProduct: string;
+  ownerID: string;
+};
+
+export async function POST(request: Request) {
+  const prisma = new PrismaClient();
+  const user = await currentUser();
+
+  const body: requestBody = (await request.json()) as requestBody;
+
+  if (user) {
+    body.ownerID = user.id;
+  }
+
+  const customer = await prisma.customer.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      mobileNumber: body.mobileNumber,
+      country: body.country,
+      city: body.city,
+      company: body.company,
+      jobTitle: body.jobTitle,
+      targetProduct: body.targetProduct,
+      ownerID: body.ownerID,
+    },
+  });
+
+  return Response.json(customer);
+}
