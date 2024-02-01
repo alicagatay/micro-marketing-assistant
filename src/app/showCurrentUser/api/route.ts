ba696ab2ad2ccb29ae32b5bc328fa8de5bd4218d
Ali Cagatay <aliccagatay@gmail.com>
2024-02-01 12:30:06 +0000

    Remove unused API route

--- src/app/showCurrentUser/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { clerkClient, currentUser } from "@clerk/nextjs";
-
-export async function GET(request: Request) {
-  const user = await currentUser();
-
-  return new Response(JSON.stringify(user));
-}
