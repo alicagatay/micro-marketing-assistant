3340db89b89ccb9d08aba353982a3f44035f3650
Ali Cagatay <aliccagatay@gmail.com>
2024-02-02 15:19:53 +0000

    Update table headers in tableScreen.tsx.

--- src/app/tableScreen.tsx
+++ src/app/tableScreen.tsx
@@ -292,6 +292,30 @@ export default function TableScreen() {
                   <tr>
                     <th
                       scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      Target Product
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      Company
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      Country
+                    </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      City
+                    </th>
+                    <th
+                      scope="col"
                       className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter sm:pl-6 lg:pl-8"
                     >
                       Name
@@ -308,36 +332,14 @@ export default function TableScreen() {
                     >
                       Mobile Number
                     </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                    >
-                      Country
-                    </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                    >
-                      City
-                    </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                    >
-                      Company
-                    </th>
+
                     <th
                       scope="col"
                       className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
                     >
                       Job Title
                     </th>
-                    <th
-                      scope="col"
-                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
-                    >
-                      Target Product
-                    </th>
+
                     <th
                       scope="col"
                       className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
@@ -354,51 +356,12 @@ export default function TableScreen() {
                           personIdx !== data.length - 1
                             ? "border-b border-gray-200"
                             : "",
-                          "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8",
-                        )}
-                      >
-                        {person.name}
-                      </td>
-                      <td
-                        className={classNames(
-                          personIdx !== data.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
-                          "hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 sm:table-cell",
-                        )}
-                      >
-                        {person.email}
-                      </td>
-                      <td
-                        className={classNames(
-                          personIdx !== data.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
-                          "hidden whitespace-nowrap px-3 py-4 text-sm text-gray-500 lg:table-cell",
-                        )}
-                      >
-                        {person.mobileNumber}
-                      </td>
-                      <td
-                        className={classNames(
-                          personIdx !== data.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
                           "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
                         )}
                       >
-                        {person.country}
+                        {person.targetProduct}
                       </td>
-                      <td
-                        className={classNames(
-                          personIdx !== data.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
-                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
-                        )}
-                      >
-                        {person.city}
-                      </td>
+
                       <td
                         className={classNames(
                           personIdx !== data.length - 1
@@ -409,6 +372,62 @@ export default function TableScreen() {
                       >
                         {person.company}
                       </td>
+
+                      <td
+                        className={classNames(
+                          personIdx !== data.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                        )}
+                      >
+                        {person.country}
+                      </td>
+
+                      <td
+                        className={classNames(
+                          personIdx !== data.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                        )}
+                      >
+                        {person.city}
+                      </td>
+
+                      <td
+                        className={classNames(
+                          personIdx !== data.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                        )}
+                      >
+                        {person.name}
+                      </td>
+
+                      <td
+                        className={classNames(
+                          personIdx !== data.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                        )}
+                      >
+                        {person.email}
+                      </td>
+
+                      <td
+                        className={classNames(
+                          personIdx !== data.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                        )}
+                      >
+                        {person.mobileNumber}
+                      </td>
+
                       <td
                         className={classNames(
                           personIdx !== data.length - 1
@@ -419,16 +438,7 @@ export default function TableScreen() {
                       >
                         {person.jobTitle}
                       </td>
-                      <td
-                        className={classNames(
-                          personIdx !== data.length - 1
-                            ? "border-b border-gray-200"
-                            : "",
-                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
-                        )}
-                      >
-                        {person.targetProduct}
-                      </td>
+
                       <td
                         className={classNames(
                           personIdx !== data.length - 1
