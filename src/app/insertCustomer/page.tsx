f59d9a0020389545f414ea981999e01a527d650a
Ali Cagatay <aliccagatay@gmail.com>
2024-02-02 14:10:23 +0000

    Fix placeholder text in customerSource input field

--- src/app/insertCustomer/page.tsx
+++ src/app/insertCustomer/page.tsx
@@ -215,7 +215,7 @@ export default function Page() {
               name="customerSource"
               id="customerSource"
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="Enter the where or how did you meet your customer.(Conference, they contacted you, LinkedIn, etc.)"
+              placeholder="Enter where or how did you meet your customer.(Conference, they contacted you, LinkedIn, etc.)"
             />
           </div>
         </div>
