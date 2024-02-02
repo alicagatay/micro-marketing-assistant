806ea06120a8241bc2f7e5b41969fb5543d4dcef
Ali Cagatay <aliccagatay@gmail.com>
2024-02-02 13:59:36 +0000

    Updated table columns based on the type changes.

--- src/app/tableScreen.tsx
+++ src/app/tableScreen.tsx
@@ -338,6 +338,12 @@ export default function TableScreen() {
                     >
                       Target Product
                     </th>
+                    <th
+                      scope="col"
+                      className="sticky top-0 z-10 border-b border-gray-300 bg-white bg-opacity-75 px-3 py-3.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter"
+                    >
+                      Source
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -422,6 +428,16 @@ export default function TableScreen() {
                         )}
                       >
                         {person.targetProduct}
+                      </td>
+                      <td
+                        className={classNames(
+                          personIdx !== data.length - 1
+                            ? "border-b border-gray-200"
+                            : "",
+                          "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
+                        )}
+                      >
+                        {person.source}
                       </td>
                     </tr>
                   ))}
