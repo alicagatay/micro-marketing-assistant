3edaa2034171b0e7ceb6fe34ef05198793cc7bb2
Ali Cagatay <aliccagatay@gmail.com>
2024-02-05 14:49:55 +0000

    Update link in tableScreen.tsx

--- src/app/tableScreen.tsx
+++ src/app/tableScreen.tsx
@@ -365,7 +365,7 @@ export default function TableScreen() {
                           "whitespace-nowrap px-3 py-4 text-sm text-gray-500",
                         )}
                       >
-                        <Link href="/">
+                        <Link href={`/customerNotes/${person.id}`}>
                           <button
                             type="button"
                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
