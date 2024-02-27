e4536bf2ade91e58a913529fc26cfc8b6e8674fe
Ali Cagatay <aliccagatay@gmail.com>
2024-02-27 09:59:18 +0000

    Add user metadata update logic to HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import TableScreen from "./tableScreen";
+import { clerkClient } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await currentUser();
+  if (user) {
+    const userId = user.id;
+    const userPrivateMetadata = (await clerkClient.users.getUser(userId))
+      .privateMetadata;
+
+    console.log(userId);
+    console.log(userPrivateMetadata);
+
+    if (userPrivateMetadata.is_test_account === undefined) {
+      await clerkClient.users.updateUser(userId, {
+        privateMetadata: { ...userPrivateMetadata, is_test_account: true },
+      });
+    }
+  }
+
   return <TableScreen />;
 }
